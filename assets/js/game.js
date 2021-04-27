65152fe800f8649775fcb10be7886c5775f8eb1d
Grant Hastings <grant.hastings96@gmail.com>
2021-04-27 16:08:52 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,17 +8,19 @@ console.log(playerName, playerAttack, playerHealth, playerMoney);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 for(var i = 0; i < enemyNames.length; i++) {
+    
+      }
     console.log(enemyNames[i]);
     console.log(i);
     console.log(enemyNames[i] + " is at " + i + " index");
-  }
+  
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 console.log(enemyNames, enemyAttack, enemyHealth)
 
 var fight = function(enemyName) {
-    while(playerHealth > 0 && enemyHealth > 0) {window.alert("Welcome to Robot Gladiators!");
+    while(playerHealth > 0 && enemyHealth > 0) {
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -53,7 +55,7 @@ if (playerHealth <= 0) {
   window.alert(playerName + " still has " + playerHealth + " health left.");
 }
 // if player choses to skip
-} else if (promptFight === "skip" || promptFight === "SKIP") {
+} if (promptFight === "skip" || promptFight === "SKIP") {
 // confirm player wants to skip
 var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
@@ -75,7 +77,15 @@ else {
         
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        // debugger;
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+    }
   }
