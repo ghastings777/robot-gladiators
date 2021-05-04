e3582422e78412b9f64d9fcb16142005734f289f
Grant Hastings <grant.hastings96@gmail.com>
2021-05-03 20:34:52 -0500

    fixed player name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,17 @@
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -17,6 +29,8 @@ var playerInfo = {
     this.money -= 7;
   }
 };
+
+
 
 // You can also log multiple values at once like this
 console.log(playerInfo.name, playerInfo.attack, playerInfo.health, playerInfo.money);
